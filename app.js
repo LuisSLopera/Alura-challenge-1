5fd2375aceb87e61765a1eb46ff24252e8751597
Luis Lopera <sistemas@zencillo.com>
2025-08-06 15:36:00 -0500

    Correccion anadir participantes

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 let amigos = [];
 let datoUsuario;
 let listaHTML = document.getElementById("listaAmigos");
-listaHTML.innerHTML = "";
+
+
 
 // Funcion que agrega a los amigos a la lista del sorteo
 function agregarAmigo() {
@@ -11,14 +12,24 @@ function agregarAmigo() {
     alert("Ingrese un dato para continuar");
     return;
   }
-  //amigos.push(datoUsuario); //Hace parte de la funcion alternativa para Ingresar usuarios
-  listaParticipantes(datoUsuario);
+  amigos.push(datoUsuario); 
+  //listaParticipantes(datoUsuario); //Hace parte de la funcion alternativa para Ingresar usuarios
   document.getElementById("amigo").value = "";
+  listaHTML.innerHTML = "";
   for (let i = 0; i < amigos.length; i++) {
-    document.createElement("li").textContent = amigos[i];
+    
+    let elementoLi=document.createElement("li");
+    elementoLi.textContent = amigos[i];
     listaHTML.appendChild(elementoLi);
   }
   console.log(amigos);
+}
+
+function sortearAmigo() {
+  if (amigos == 0) {
+    alert("No se puede sortear, no hay participantes");
+    return;
+  }
 }
 
 /* Funcion alternativa para Ingresar usuarios
