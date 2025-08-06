8d0093f4a8f57f6c8d404212f2801601fb0b10b9
Luis Lopera <sistemas@zencillo.com>
2025-08-06 15:25:55 -0500

    Funcionalidad para insertar los datos en una lista

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
+// Variables
 let amigos = [];
 let datoUsuario;
+let listaHTML = document.getElementById("listaAmigos");
+listaHTML.innerHTML = "";
 
+// Funcion que agrega a los amigos a la lista del sorteo
 function agregarAmigo() {
   datoUsuario = document.getElementById("amigo").value;
   if (datoUsuario == "" || datoUsuario == null) {
     alert("Ingrese un dato para continuar");
     return;
   }
-  amigos.push(datoUsuario);
+  //amigos.push(datoUsuario); //Hace parte de la funcion alternativa para Ingresar usuarios
+  listaParticipantes(datoUsuario);
   document.getElementById("amigo").value = "";
+  for (let i = 0; i < amigos.length; i++) {
+    document.createElement("li").textContent = amigos[i];
+    listaHTML.appendChild(elementoLi);
+  }
   console.log(amigos);
 }
+
+/* Funcion alternativa para Ingresar usuarios
+function listaParticipantes(dato) {
+  let elementoLi = document.createElement("li");
+  elementoLi.textContent=dato;
+  listaHTML.appendChild(elementoLi);
+}
+ */
